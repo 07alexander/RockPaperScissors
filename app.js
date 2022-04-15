b091ac4de69c357405085c3ea2e57e60b133b655
07alexander <07alexander@gmail.com>
2022-04-15 14:38:29 -0700

    playRound is now functional!

--- app.js
+++ app.js
@@ -1,29 +1,53 @@
 // computerPlay will randomly return either rock, paper, or scissors
 function computerPlay(){
     var randNum = 0;
-    randNum = Math.floor(Math.random() * 3);
-    console.log(randNum);
-    switch (randNum){
-        case 0:
-            return "rock";
-            break;
-        case 1:
-            return "paper";
-            break;
-        case 2:
-            return "scissors";
-            break;
-        default:
-            console.log("Computer play error!");
-    }
+    return Math.floor(Math.random() * 3);
 }
 
 //This function will play a single round of Rock Paper Scissors and declare the winner
 function playRound(playerSelection, computerSelection){
-    const winner="";
-
+    var winner;
+    var playerInput;
+    
+    switch (playerSelection.toUpperCase()){
+        case "ROCK":
+            playerInput=0;
+            if(computerSelection === 2){
+                winner = "PLAYER WINS!";
+            }else if(computerSelection === 1){
+                winner = "COMPUTER WINS!";
+            }else{
+                winner = "TIE GAME!";
+            }
+            break;
+        case "PAPER":
+            playerInput=1;
+            if(computerSelection === 0){
+                winner = "PLAYER WINS!";
+            }else if(computerSelection === 2){
+                winner = "COMPUTER WINS!";
+            }else{
+                winner = "TIE GAME!";
+            }
+            break;
+        case "SCISSORS":
+            playerInput=2;
+            if(computerSelection === 1){
+                winner = "PLAYER WINS!";
+            }else if(computerSelection === 0){
+                winner = "COMPUTER WINS!";
+            }else{
+                winner = "TIE GAME!";
+            }
+            break;
+        default:
+            console.log("PLAYER ERROR!!!");
+    };
     return winner;
 }
 
-console.log(computerPlay());
+const playerSelection = "paper";
+const computerSelection = computerPlay();
 
+console.log(playerSelection, computerSelection);
+console.log(playRound (playerSelection, computerSelection));
